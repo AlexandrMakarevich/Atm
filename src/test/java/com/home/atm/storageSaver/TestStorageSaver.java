df98333da7641ff87f73af1519967153f55b1ff1
Alexandr.Makarevich <alexandr.makarevich88@gmail.com>
2017-03-24 21:39:39 +0200

    fixed all tests in TestService

--- src/test/java/com/home/atm/storageSaver/TestStorageSaver.java
+++ src/test/java/com/home/atm/storageSaver/TestStorageSaver.java
@@ -9,30 +9,31 @@ import org.junit.Test;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
-import static com.home.atm.storage.StorageSaver.filename;
 
 public class TestStorageSaver {
 
     private StorageSaver storageSaver;
     private TestStorageLoader testStorageLoader;
-    private String actualFile = "src/test/resources/Storage.txt";
+    private String expectedFile = "src/test/resources/Storage.txt";
+    public static String file = "target/Save_account.txt";
 
     @Before
     public void init() {
         storageSaver = new StorageSaver();
         testStorageLoader = new TestStorageLoader();
+        StorageSaver.filename = file;
     }
 
     @Test
     public void testStorageSaver() throws FileNotFoundException {
         Storage storage = testStorageLoader.createStorage();
         storageSaver.writeData(storage);
-        List<String> actualList = substractFile(actualFile);
-        List<String> expeList = substractFile(filename);
-        Assert.assertEquals("Actual result must be expected", expeList, actualList);
+        List<String> actualList = loadFile(file);
+        List<String> expectedList = loadFile(expectedFile);
+        Assert.assertEquals("Actual result must be expected", expectedList, actualList);
     }
 
-    public List<String> substractFile(String fileName) throws FileNotFoundException {
+    public List<String> loadFile(String fileName) throws FileNotFoundException {
         File file = new File(fileName);
         Scanner scanner = new Scanner(file);
         List<String> list = new ArrayList<>();
