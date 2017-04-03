5857fd32848f4c781d0ce96b26df10487a4fd130
AlexandrMakarevich <alexandr.makarevich88@gmail.com>
2017-04-03 15:43:55 +0300

    added load Storage with account

--- src/main/java/com/home/atm/AtmLauncher.java
+++ src/main/java/com/home/atm/AtmLauncher.java
@@ -1,12 +1,16 @@
 package com.home.atm;
 
 import com.home.atm.command.ServiceAccount;
+import com.home.atm.storage.Storage;
+import com.home.atm.storage.StorageLoader;
 import java.io.FileNotFoundException;
 
 public class AtmLauncher {
 
     public static void main(String[] args) throws FileNotFoundException {
+        StorageLoader storageLoader = new StorageLoader();
+        Storage storage = storageLoader.loadStorage("Save_account.txt");
         ServiceAccount serviceAccount = new ServiceAccount();
-        serviceAccount.launchAccount();
+        serviceAccount.launchAccount(storage);
     }
 }
--- src/main/java/com/home/atm/command/ServiceAccount.java
+++ src/main/java/com/home/atm/command/ServiceAccount.java
@@ -2,29 +2,19 @@ package com.home.atm.command;
 
 import com.home.atm.PreparationAtm;
 import com.home.atm.storage.Storage;
-import com.home.atm.storage.StorageLoader;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
-import static com.home.atm.storage.StorageSaver.filename;
 
 public class ServiceAccount {
 
-    private StorageLoader storageLoader = new StorageLoader();
     private PreparationAtm preparationAtm = new PreparationAtm();
 
-    public void launchAccount()  {
+    public void launchAccount(Storage storage) {
         System.out.println("Enter the account number :");
         Scanner in = new Scanner(System.in);
         while (in.hasNext()) {
-            try{
-                String account = in.nextLine();
-                filename = account;
-                Storage storage = storageLoader.loadStorage(account);
-                preparationAtm.process(storage);
-            }
-            catch(FileNotFoundException ex) {
-                System.out.println("Account does not exist.\nEnter correct account number :");
-            }
+            String account = in.nextLine();
+            storage.setCurrentAccount(account);
+            preparationAtm.process(storage);
         }
     }
 }
