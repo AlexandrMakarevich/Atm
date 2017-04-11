26d25f73378c0527c3a13413aace73ec9ceedd1d
Alexey Dranchuk <alexey.dranchuk@opower.com>
2017-04-11 22:06:16 +0300

    fix for broken mavne tests

--- src/main/java/com/home/atm/storage/StorageConstant.java
+++ src/main/java/com/home/atm/storage/StorageConstant.java
@@ -5,13 +5,17 @@ public class StorageConstant {
     private final String JSON_FORMAT = "json";
     private final String CSV_FORMAT = "csv";
     public static final String STORAGE_TYPE = "storageType";
-    public static final String STORAGE_TYPE_PROPERTY = System.getProperty(STORAGE_TYPE);
 
     public boolean isJSON() {
-        return JSON_FORMAT.equals(STORAGE_TYPE_PROPERTY);
+        return JSON_FORMAT.equals(getStorageTypeProperty());
     }
 
     public boolean isCSV() {
-        return CSV_FORMAT.equals(STORAGE_TYPE_PROPERTY);
+        return CSV_FORMAT.equals(getStorageTypeProperty());
     }
+
+    private String getStorageTypeProperty() {
+        return System.getProperty(STORAGE_TYPE);
+    }
+
 }
